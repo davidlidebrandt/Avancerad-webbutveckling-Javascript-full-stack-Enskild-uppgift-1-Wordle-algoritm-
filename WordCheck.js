cec8f228800745aa438d4adc7b37076dd0549e26
David Lidebrandt <david.lidebrandt@gmail.com>
2023-03-20 14:02:50 +0100

    Rewrite function for matching the letters

--- WordCheck.js
+++ WordCheck.js
@@ -1,27 +1,39 @@
-export default function wordCheck(inputWord, correctWord) {
-    const checkedLetters = [];
-    const lettersWithCorrectPlacement = [];
-    for(let i=0; i<inputWord.length; i++) {
-        const currentLetter = inputWord[i];
-        const correctLetter= correctWord.includes(currentLetter);
-        const correctPlacement = correctWord.indexOf(currentLetter) === i;
-        if(correctLetter && correctPlacement) {
-            const obj = {};
-            obj[currentLetter] = "correct"
-            checkedLetters.push(obj);
-        } 
-        else if(correctLetter){
-            const obj = {};
-            obj[currentLetter] = "misplaced";
-            checkedLetters.push(obj);
-        } else {
-            const obj = {};
-            obj[currentLetter] = "incorrect";
-            checkedLetters.push(obj);
+export default function wordCheck(inputWord, correctWord){
+    const checkedLetters = Array(inputWord.length).fill({});
+    correctWord = correctWord.split("");
+    inputWord = inputWord.split("");
+    
+    // Find all letters at correct positions and mark them as 'found'
+    // to ensure they won't be counted more than once.
+    inputWord.forEach((letter, index) => {
+        const currentLetter = letter;
+        if(correctWord[index] === inputWord[index]) {
+            const letterAndValue = {};
+            letterAndValue[currentLetter.toUpperCase()] = "correct";
+            checkedLetters[index] = letterAndValue;
+            inputWord[index] = null;
+            correctWord[index] = null;
         }
-    }
+    });
+
+    // Find misplaced letters and mark them as 'found'
+    // to ensure they won't be counted more than once.
+    // Mark all other letters as incorrect.
+    inputWord.forEach((letter, index)=> {
+        const currentLetter = letter;
+        if(letter != null && correctWord.includes(letter)) {
+            const letterAndValue = {};
+            letterAndValue[currentLetter.toUpperCase()] = "misplaced";
+            checkedLetters[index] = letterAndValue;
+            inputWord[index] = null;
+            correctWord[index] = null;
+        }
+        else if(letter != null) {
+            const letterAndValue = {};
+            letterAndValue[currentLetter.toUpperCase()] = "incorrect";
+            checkedLetters[index] = letterAndValue;
+        }
+    });
+    
     return checkedLetters;
 }
-
-const t = wordCheck("HALLÅ", "CYKLA"); 
-console.log(t)
--- WordCheck.test.js
+++ WordCheck.test.js
@@ -11,9 +11,19 @@ import {
 
   describe("wordCheck()", ()=> {
 
-    test("Should return incorrect for the first letter", ()=> {
-        const result = wordCheck("word", "sword");
-        expect(result[0]).toBe("incorrect");
+    test("Should return object with key 'W' and value 'incorrect' for the first letter", ()=> {
+        const result = wordCheck("words", "lords");
+        expect(result[0]).toMatchObject({"W": "incorrect"});
     });
+
+    test("Should return object with key 'D' and value 'correct' for the last letter", ()=> {
+        const result = wordCheck("words", "lords");
+        expect(result[4]).toMatchObject({"S": "correct"});
+    });
+
+    test("Should return object with key 'G' and value 'misplaced' for the first letter", ()=> {
+      const result = wordCheck("great", "signs");
+      expect(result[0]).toMatchObject({"G": "misplaced"});
+  });
 
   });
